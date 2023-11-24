64f82e48cfe2967468c6190f73c6108a57561ff5
Yui Nishimura <nisshi.yui79@gmail.com>
2023-11-24 16:51:07 +0900

    Add print function and update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,47 @@
-int main(int argc, char const *argv[]) {
-	/* code */
-	return 0;
+
+#include <iostream>
+
+template <typename T> void print(T t)
+{
+	std::cout << t << std::endl;
 }
+
+int main(int argc, char const *argv[])
+{
+	print("Hello World!");
+	print(1);
+	return 0;
+	const Config config = {
+		"name" : "test",
+	};
+	const Config config = parse.then((result) = > ({
+													... result,
+													"name" : "test",
+												})) const Config config(parse.then((result) = > ({
+																								  ... result,
+																								  "name" : "test",
+																							  })))
+
+							  const Config config;
+	config = parse.then((result) = > ({
+									   ... result,
+									   "name" : "test",
+								   }))
+}
+
+struct Readonly
+{
+	int a;
+
+  private:
+	const Readonly &operator=(const Readonly &other);
+};
+
+struct Config : public Readonly
+{
+  public:
+	Config(const Config &config);
+	Chat	chat;
+	int		a;
+	long	pp;
+};
