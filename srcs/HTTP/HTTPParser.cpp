c201aa2886d8797901c9949ef7c64f8d828fa412
Minato Abe <m.abe.howcollect@gmail.com>
2023-12-16 13:59:28 +0900

    feat: req messageパース

--- srcs/HTTP/HTTPParser.cpp
+++ srcs/HTTP/HTTPParser.cpp
@@ -1,11 +1,14 @@
 #include "HTTPParser.hpp"
 
-HTTPParser::HTTPParser(const std::string &request)
+HTTPParser::HTTPParser(std::string &request)
 {
-	parse(request);
+	executeParse(request);
 }
 
-HTTPParser::HTTPParser(const HTTPParser &src) {}
+HTTPParser::HTTPParser(const HTTPParser &src)
+{
+	(*this) = src;
+}
 
 HTTPParser	&HTTPParser::operator=(const HTTPParser &rhs)
 {
@@ -18,25 +21,70 @@ HTTPParser	&HTTPParser::operator=(const HTTPParser &rhs)
 
 HTTPParser::~HTTPParser(void) {}
 
+std::string	HTTPParser::getMethod(void) const
+{
+	return (_method);
+}
+
+std::string	HTTPParser::getTarget(void) const
+{
+	return (_url);
+}
+
+std::string	HTTPParser::getVersion(void) const
+{
+	return (_version);
+}
+
 static std::string	getRequestLine(std::string &data)
 {
 	std::string		request_line;
 	size_t			pos;
 
-	pos = data.find("\r\n");
+	pos = data.find("\n");
 	if (pos == std::string::npos)
+	{
+		std::cout << "here" << std::endl; // debug
 		return (request_line);
+	}
 	request_line = data.substr(0, pos);
-	data.erase(0, pos + 2);
+	data.erase(0, pos + 1);
 	return (request_line);
 }
+
+// static std::string	getHeader(std::string &data)
+// {
+// 	std::string		line;
+// 	size_t			pos;
+
+// 	std::istringstream	iss(data);
+// 	while (std::getline(iss, line))
+// 	{
+// 		if (strlen(line.c_str()) == 0)
+// 			break ;
+// 		/* _headerに格納していく */
+		
+// 		/* dataからlineを削除していく */
+// 	}
+// 	return (header);
+// }
 
 void	HTTPParser::executeParse(std::string &data)
 {
 	std::string		request_line;
 
+	std::cout << "[data]\n" << data << std::endl; // debug
 	request_line = getRequestLine(data);
+	std::cout << "request_line: " << request_line << std::endl; // debug
 	/* メソッドとターゲット、バージョンを格納する(error処理も) */
+	std::istringstream	iss(request_line);
+	iss >> _method >> _url >> _version;
+
+
 	/* ヘッダーを格納する(error処理も) */
+	std::cout << "[data]\n" << data << std::endl; // debug
+
+
 	/* ボディを格納する(error処理も) */
+	std::cout << "[data] \n" << data << std::endl; // debug
 }
--- srcs/HTTP/HTTPParser.hpp
+++ srcs/HTTP/HTTPParser.hpp
@@ -4,6 +4,7 @@
 # include <cstring>
 # include <unistd.h>
 # include <iostream>
+# include <sstream>
 # include <map>
 
 class HTTPParser
@@ -11,23 +12,97 @@ class HTTPParser
 	private:
 		enum state
 		{
-			e_request_line,
-			e_header,
-			e_body,
-			e_done
+			e_dead = 1,
+			e_start_req_or_res,
+			e_res_or_resp_H,
+			e_start_res,
+			e_res_H,
+			e_res_HT,
+			e_res_HTT,
+			e_res_HTTP,
+			e_res_http_major,
+			e_res_http_dot,
+			e_res_http_minor,
+			e_res_http_end,
+			e_res_first_status_code,
+			e_res_status_code,
+			e_res_status_start,
+			e_res_status,
+			e_res_line_almost_done,
+
+			e_start_req,
+
+			e_req_method,
+			e_req_spaces_before_url,
+			e_req_schema,
+			e_req_schema_slash,
+			e_req_schema_slash_slash,
+			e_req_server_start,
+			e_req_server,
+			e_req_server_with_at,
+			e_req_path,
+			e_req_query_string_start,
+			e_req_query_string,
+			e_req_fragment_start,
+			e_req_fragment,
+			e_req_http_start,
+			e_req_http_H,
+			e_req_http_HT,
+			e_req_http_HTT,
+			e_req_http_HTTP,
+			e_req_http_I,
+			e_req_http_IC,
+			e_req_http_major,
+			e_req_http_dot,
+			e_req_http_minor,
+			e_req_http_end,
+			e_req_line_almost_done,
+
+			e_header_field_start,
+			e_header_field,
+			e_header_value_discard_ws,
+			e_header_value_discard_ws_almost_done,
+			e_header_value_discard_lws,
+			e_header_value_start,
+			e_header_value,
+			e_header_value_lws,
+
+			e_header_almost_done,
+
+			e_chunk_size_start,
+			e_chunk_size,
+			e_chunk_parameters,
+			e_chunk_size_almost_done,
+
+			e_headers_almost_done,
+			e_headers_done,
+
+			e_chunk_data,
+			e_chunk_data_almost_done,
+			e_chunk_data_done,
+
+			e_body_identity,
+			e_body_identity_eof,
+
+			e_message_done
 		};
-		state								_state;
+		// state								_state;
 		std::string							_method;
+		std::string							_version;
 		std::string							_url;
 		std::map<std::string, std::string>	_header;
 		std::string							_body;
-		int									_error_code;
+		// int									_error_code;
 
 	public:
-		HTTPParser(const std::string &request);
+		HTTPParser(std::string &request);
 		HTTPParser(const HTTPParser &src);
 		HTTPParser	&operator=(const HTTPParser &rhs);
 		~HTTPParser(void);
+
+		std::string	getMethod(void) const;
+		std::string	getTarget(void) const;
+		std::string	getVersion(void) const;
 
 		void	executeParse(std::string &data);
 };
