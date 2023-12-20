6d84a48a4e25929b4db439116f30357f950ae38a
Subarunrun <86033765+subarunrun0812@users.noreply.github.com>
2023-12-21 03:02:13 +0900

    add: [エラー処理]allow_method

--- srcs/config/parse_config.cpp
+++ srcs/config/parse_config.cpp
@@ -118,17 +118,11 @@ template <typename T>
 T Config::PullWord(std::istringstream& iss)
 {
 	T word;
-	if (iss >> word)
-	{
-		;
+	if (!(iss >> word)) {
+		std::ostringstream oss;
+		oss << "Config: ディレクティブの引数が不正です: ";
+		throw std::runtime_error(oss.str());
 	}
-	else
-	{
-		// std::cout << "\x1b[31mwordが取得できませんでした\x1b[0m" << std::endl;
-		throw("\x1b[31mconfig:wordが取得できませんでした\x1b[0m");
-		return T();
-	}
-
 	return word;
 }
 
@@ -144,70 +138,84 @@ void Config::ParseLocation(std::ifstream& config_file, Location& location)
 		std::istringstream iss(line);
 		std::string key;
 		iss >> key;
-
-		// 各ディレクティブに対する処理を記述
-		if (key == "root")
+		try
 		{
-			location.root = PullWord<std::string>(iss);
-		}
-		else if (key == "autoindex")
-		{
-			std::string autoindex_value = PullWord<std::string>(iss);
-			location.autoindex = (autoindex_value == "on");
-		}
-		else if (key == "index")
-		{
-			location.index = PullWord<std::string>(iss);
-		}
-		else if (key == "client_max_body_size")
-		{
-			location.client_max_body_size = PullWord<size_t>(iss);
-		}
-		else if (key == "error_page")
-		{
-			int error_code = PullWord<int>(iss);
-			std::string error_page_path = PullWord<std::string>(iss);
-			location.error_page[error_code] = error_page_path;
-		}
-		else if (key == "allow_method")
-		{
-			// 許可されるHTTPメソッドをvectorに追加
-			std::string method;
-			while (iss >> method)
+			// 各ディレクティブに対する処理を記述
+			if (key == "root")
 			{
-				location.allow_method.push_back(method);
+				location.root = PullWord<std::string>(iss);
+			}
+			else if (key == "autoindex")
+			{
+				std::string autoindex_value = PullWord<std::string>(iss);
+				location.autoindex = (autoindex_value == "on");
+			}
+			else if (key == "index")
+			{
+				location.index = PullWord<std::string>(iss);
+			}
+			else if (key == "client_max_body_size")
+			{
+				location.client_max_body_size = PullWord<size_t>(iss);
+			}
+			else if (key == "error_page")
+			{
+				int error_code = PullWord<int>(iss);
+				std::string error_page_path = PullWord<std::string>(iss);
+				location.error_page[error_code] = error_page_path;
+			}
+			else if (key == "allow_method")
+			{
+				// 許可されるHTTPメソッドをvectorに追加
+				std::string method;
+				int i = 0;
+				while (iss >> method)
+				{
+					if (method != "GET" && method != "POST" && method != "DELETE")
+						throw(std::runtime_error("Config: 許可されないHTTPメソッドが指定されています"));
+					location.allow_method.push_back(method);
+					i++;
+				}
+				if (i > 3)
+					throw(std::runtime_error("Config: 許可されるHTTPメソッドが多すぎます"));
+			}
+			else if (key == "cgi_path")
+			{
+				location.cgi_path = PullWord<std::string>(iss);
+			}
+			else if (key == "upload_path")
+			{
+				location.upload_path = PullWord<std::string>(iss);
+			}
+			else if (key == "return")
+			{
+				int status_code = PullWord<int>(iss);  // ステータスコードを取得
+				std::string redirect_url = PullWord<std::string>(iss);  // リダイレクトURLを取得
+				location.redirect[status_code] = redirect_url;  // マップに追加
+			}
+			else if (key == "}")
+			{
+				break; // locationブロックの終了
+			}
+			else if (key.empty() || key == "\t" || key == "\n")
+			{
+				continue;
+			}
+			else
+			{
+				std::string next_line;
+				std::getline(config_file, next_line);
+				std::cout << RED << "次の行: " << next_line << NORMAL << std::endl;
+				std::getline(config_file, next_line);
+				std::cout << RED << "次の行: " << next_line << NORMAL << std::endl;
+				throw std::runtime_error(("不正なディレクティブ"));
 			}
 		}
-		else if (key == "cgi_path")
+		catch (const std::runtime_error& e)
 		{
-			location.cgi_path = PullWord<std::string>(iss);
-		}
-		else if (key == "upload_path")
-		{
-			location.upload_path = PullWord<std::string>(iss);
-		}
-		else if (key == "return")
-		{
-			int status_code = PullWord<int>(iss);  // ステータスコードを取得
-			std::string redirect_url = PullWord<std::string>(iss);  // リダイレクトURLを取得
-			location.redirect[status_code] = redirect_url;  // マップに追加
-		}
-		else if (key == "}")
-		{
-			break; // locationブロックの終了
-		}
-		else if (key.empty() || key == "\t" || key == "\n")
-		{
-			continue;
-		}
-		else
-		{
-			std::string next_line;
-			std::getline(config_file, next_line);
-			std::cout << RED << "次の行: " << next_line << std::endl;
-			std::getline(config_file, next_line);
-			std::cout << RED << "次の行: " << next_line << std::endl;
-			throw("不正なディレクティブ");
+			// 正常に解析が行われなkった部分を発見する
+			std::cerr << RED << e.what() << "\nkey: " << key << NORMAL << std::endl;
+			throw(std::runtime_error(""));
 		}
 	}
 }
@@ -226,63 +234,72 @@ void Config::ParseServer(std::ifstream& config_file, Server& server)
 		std::string key;
 		iss >> key;
 
-		// 各ディレクティブに対する処理を記述
-		if (key == "server_name")
+		try
 		{
-			server.server_name = PullWord<std::string>(iss);
+
+			// 各ディレクティブに対する処理を記述
+			if (key == "server_name")
+			{
+				server.server_name = PullWord<std::string>(iss);
+			}
+			else if (key == "listen")
+			{
+				server.port = PullWord<int>(iss);
+			}
+			else if (key == "location")
+			{
+				Location location;
+				InitializeLocation(location);
+				location.path = PullWord<std::string>(iss);
+				ParseLocation(config_file, location);
+				server.locations.push_back(location);
+			}
+			else if (key == "root")
+			{
+				server.root = PullWord<std::string>(iss);
+			}
+			else if (key == "error_page")
+			{
+				int error_code = PullWord<int>(iss);
+				std::string error_page_path = PullWord<std::string>(iss);
+				server.error_page[error_code] = error_page_path;
+			}
+			else if (key == "client_max_body_size")
+			{
+				server.client_max_body_size = PullWord<size_t>(iss);
+			}
+			else if (key == "autoindex")
+			{
+				std::string autoindex_value = PullWord<std::string>(iss);
+				server.autoindex = (autoindex_value == "on");
+			}
+			else if (key == "index")
+			{
+				server.index = PullWord<std::string>(iss);
+			}
+			else if (key == "}")
+			{
+				break;
+			}
+			else if (key.empty() || key == "\t" || key == "\n")
+			{
+				continue;
+			}
+			else
+			{
+				std::string next_line;
+				std::getline(config_file, next_line);
+				std::cout << RED << "次の行: " << next_line << std::endl;
+				std::getline(config_file, next_line);
+				std::cout << RED << "次の行: " << next_line << std::endl;
+				throw std::runtime_error(("Config: 不正なディレクティブ"));
+			}
 		}
-		else if (key == "listen")
+		catch (const std::runtime_error& e)
 		{
-			server.port = PullWord<int>(iss);
-		}
-		else if (key == "location")
-		{
-			Location location;
-			InitializeLocation(location);
-			location.path = PullWord<std::string>(iss);
-			ParseLocation(config_file, location);
-			server.locations.push_back(location);
-		}
-		else if (key == "root")
-		{
-			server.root = PullWord<std::string>(iss);
-		}
-		else if (key == "error_page")
-		{
-			int error_code = PullWord<int>(iss);
-			std::string error_page_path = PullWord<std::string>(iss);
-			server.error_page[error_code] = error_page_path;
-		}
-		else if (key == "client_max_body_size")
-		{
-			server.client_max_body_size = PullWord<size_t>(iss);
-		}
-		else if (key == "autoindex")
-		{
-			std::string autoindex_value = PullWord<std::string>(iss);
-			server.autoindex = (autoindex_value == "on");
-		}
-		else if (key == "index")
-		{
-			server.index = PullWord<std::string>(iss);
-		}
-		else if (key == "}")
-		{
-			break;
-		}
-		else if (key.empty() || key == "\t" || key == "\n")
-		{
-			continue;
-		}
-		else
-		{
-			std::string next_line;
-			std::getline(config_file, next_line);
-			std::cout << RED << "次の行: " << next_line << std::endl;
-			std::getline(config_file, next_line);
-			std::cout << RED << "次の行: " << next_line << std::endl;
-			throw("不正なディレクティブ");
-			break;
+			// 正常に解析が行われなkった部分を発見する
+			std::cerr << RED << e.what() << "\nkey: " << key << NORMAL << std::endl;
+			throw(std::runtime_error(""));
 		}
 	}
 }
@@ -333,9 +350,9 @@ int main(int argc, char** argv)
 			config.PrintServer(config.webserver.servers[i]);
 		}
 	}
-	catch (const std::exception& e)
+	catch (const std::runtime_error& e)
 	{
-		std::cerr << e.what() << '\n';
+		std::cerr << RED << e.what() << NORMAL;
 		return 1;
 	}
 
