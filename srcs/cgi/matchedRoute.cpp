7e694bc6430871ff2282af36f7f043640fdb267c
Yui Nishimura <nisshi.yui79@gmail.com>
2023-12-24 15:20:28 +0900

    Add lengthOfPrefixMatch function to calculate the length of prefix match

--- srcs/cgi/matchedRoute.cpp
+++ srcs/cgi/matchedRoute.cpp
@@ -1,10 +1,19 @@
 #include "../config/serverConfig.hpp"
 
+unsigned int lengthOfPrefixMatch(const std::string string, const std::string pattern)
+{
+	unsigned int i = 0;
+	while (i < string.length() && i < pattern.length() && string[i] == pattern[i])
+		i++;
+	return i;
+}
+
+
 RouteConfig matchedRoute(const std::string uri, const std::vector<RouteConfig> routes)
 {
 	for (unsigned int i = 0; i < routes.size(); i++)
 	{
-		if (uri.find(routes[i].) != std::string::npos)
+		if (uri.find(routes[i].) == 0)
 			return routes[i];
 	}
 	return RouteConfig();
