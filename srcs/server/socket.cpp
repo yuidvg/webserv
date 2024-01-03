885821ec3e2ea1e9a0c3ecba3b1dd70cd82aabaa
Subaru Sasaki <subarunrun0812@yahoo.co.jp>
2024-01-03 17:20:18 +0900

    update: errnoを使用している箇所を削除

--- srcs/server/socket.cpp
+++ srcs/server/socket.cpp
@@ -1,40 +1,43 @@
 #include "socket.hpp"
 
-
-// TODO: errnoは使用禁止なため、コードを修正する必要がある
-
-Server::Server() {
-
+Server::Server()
+{
 }
 
-Server::~Server() {
-
+Server::~Server()
+{
 }
 
-
-void Server::CloseConnection(int socket) {
+void Server::CloseConnection(int socket)
+{
 	close(socket);
 	FD_CLR(socket, &master_set);
-	if (socket == max_sd) {
-		while (!FD_ISSET(max_sd, &master_set)) {
+	if (socket == max_sd)
+	{
+		while (!FD_ISSET(max_sd, &master_set))
+		{
 			max_sd -= 1;
 		}
 	}
 }
 
-void Server::InitializeSocket(ConfigServer config_server) {
+void Server::InitializeSocket(ConfigServer config_server)
+{
 	listen_sd = socket(PF_INET, SOCK_STREAM, 0);
-	if (listen_sd < 0) {
+	if (listen_sd < 0)
+	{
 		throw std::runtime_error("socket() failed");
 	}
 
 	int flags = fcntl(listen_sd, F_GETFL, 0);
-	if (flags < 0) {
+	if (flags < 0)
+	{
 		close(listen_sd);
 		throw std::runtime_error("fcntl() failed");
 	}
 	flags |= O_NONBLOCK;
-	if (fcntl(listen_sd, F_SETFL, flags) < 0) {
+	if (fcntl(listen_sd, F_SETFL, flags) < 0)
+	{
 		close(listen_sd);
 		throw std::runtime_error("fcntl() failed to set non-blocking");
 	}
@@ -42,14 +45,16 @@ void Server::InitializeSocket(ConfigServer config_server) {
 	struct sockaddr_in addr = {};
 	addr.sin_family = PF_INET;
 	addr.sin_addr.s_addr = htonl(INADDR_ANY);  // IPv4アドレスを指定
-	addr.sin_port = htons(config_server.port);  // ポート番号を設定
+	addr.sin_port = htons(config_server.port); // ポート番号を設定
 
-	if (bind(listen_sd, (struct sockaddr*)&addr, sizeof(addr)) < 0) {
+	if (bind(listen_sd, (struct sockaddr *)&addr, sizeof(addr)) < 0)
+	{
 		close(listen_sd);
 		throw std::runtime_error("bind() failed");
 	}
 
-	if (listen(listen_sd, 5) < 0) {
+	if (listen(listen_sd, 5) < 0)
+	{
 		close(listen_sd);
 		throw std::runtime_error("listen() failed");
 	}
@@ -60,40 +65,43 @@ void Server::InitializeSocket(ConfigServer config_server) {
 	max_sd = listen_sd;
 }
 
-
-void Server::AcceptNewConnection() {
+void Server::AcceptNewConnection()
+{
 	int new_sd = accept(listen_sd, NULL, NULL);
-	if (new_sd < 0) {
-		if (errno != EWOULDBLOCK) {
-			// ノンブロッキング操作がブロックされた
-			throw std::runtime_error("accept() failed");
-		}
+	if (new_sd < 0)
+	{
+		// ノンブロッキング操作がブロックされた
+		throw std::runtime_error("accept() failed");
 	}
-	else {
+	else
+	{
 		FD_SET(new_sd, &master_set);
-		if (new_sd > max_sd) {
+		if (new_sd > max_sd)
+		{
 			max_sd = new_sd;
 		}
 	}
 }
 
 // 接続が確立されたソケットと通信する
-void Server::ProcessConnection(int socket) {
+void Server::ProcessConnection(int socket)
+{
 	bool close_conn = false;
 	char buffer[80];
 	int rc;
 
-	while (true) {
+	while (true)
+	{
 		rc = recv(socket, buffer, sizeof(buffer), 0);
-		if (rc < 0) {
-			if (errno != EWOULDBLOCK) {
-				std::cerr << "recv() failed: " << strerror(errno) << std::endl;
-				close_conn = true;
-			}
+		if (rc < 0)
+		{
+			std::cerr << "recv() failed: " << std::endl;
+			close_conn = true;
 			break;
 		}
 
-		if (rc == 0) {
+		if (rc == 0)
+		{
 			std::cout << "Connection closed" << std::endl;
 			close_conn = true;
 			break;
@@ -109,62 +117,69 @@ void Server::ProcessConnection(int socket) {
 		// HTTPレスポンス作成のロジックをここに実装
 		std::string response = "HTTP/1.1 200 OK\r\nContent-Length: 22\r\n\r\n<h1>Hello Webserv</h1>";
 		rc = send(socket, response.c_str(), response.length(), 0);
-		if (rc < 0) {
-			std::cerr << "send() failed: " << strerror(errno) << std::endl;
+		if (rc < 0)
+		{
+			std::cerr << "send() failed: " << std::endl;
 			close_conn = true;
 			break;
 		}
 	}
 
-	if (close_conn) {
+	if (close_conn)
+	{
 		CloseConnection(socket);
 	}
 }
 
-
-
-
-
-
-int Server::Start(ConfigServer config_server) {
+int Server::Start(ConfigServer config_server)
+{
 	InitializeSocket(config_server);
 
 	fd_set working_set;
 	struct timeval timeout;
 	int end_server = FALSE;
 
-	while (end_server == FALSE) {
+	while (end_server == FALSE)
+	{
 		memcpy(&working_set, &master_set, sizeof(master_set));
 		timeout.tv_sec = 3 * 60; // タイムアウト値を3分に設定
 		timeout.tv_usec = 0;
 
 		std::cout << "Waiting on select()..." << std::endl;
 		int rc = select(max_sd + 1, &working_set, NULL, NULL, &timeout);
-		if (rc < 0) {
-			std::cerr << "select() failed: " << strerror(errno) << std::endl;
+		if (rc < 0)
+		{
+			std::cerr << "select() failed: " << std::endl;
 			break;
 		}
-		else if (rc == 0) {
+		else if (rc == 0)
+		{
 			std::cout << "select() timed out. End program." << std::endl;
 			break;
 		}
 
 		// std::cout << "max_sd" << max_sd << std::endl;
-		for (int i = 0; i <= max_sd; ++i) {
+		for (int i = 0; i <= max_sd; ++i)
+		{
 			// working_setの集合に特定のFD(i)が存在するかどうかを判別する
-			if (FD_ISSET(i, &working_set)) {
-				if (i == listen_sd) {
+			if (FD_ISSET(i, &working_set))
+			{
+				if (i == listen_sd)
+				{
 					AcceptNewConnection();
 				}
-				else {
+				else
+				{
 					ProcessConnection(i);
 				}
 			}
 		}
 	}
 
-	for (int i = 0; i <= max_sd; ++i) {
-		if (FD_ISSET(i, &master_set)) {
+	for (int i = 0; i <= max_sd; ++i)
+	{
+		if (FD_ISSET(i, &master_set))
+		{
 			CloseConnection(i);
 		}
 	}
