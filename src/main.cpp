43ddf8a41a4277558c1da5e226848cf0b28b99b2
Minato Abe <m.abe.howcollect@gmail.com>
2024-01-08 17:24:40 +0900

    add: main

---
+++ src/main.cpp
@@ -0,0 +1,33 @@
+#include "server/connection.hpp"
+#include "config/parseConfig.hpp"
+
+int main(int argc, char **argv)
+{
+	const char *config_path;
+	if (argc < 2)
+		config_path = "../config/default.conf";
+	else if (argc == 2)
+		config_path = argv[1];
+	else
+	{
+		std::cerr << RED << "引数が多すぎます" << NORMAL << std::endl;
+		return 1;
+	}
+
+	ParseResult result = ParseConfig(config_path);
+	if (!result.ok())
+	{
+		std::cerr << RED << result.unwrapErr() << NORMAL << std::endl;
+		return 1;
+	}
+	std::vector<Server> servers = result.unwrap();
+	// DebugPrint(servers);
+
+	Connection connection;
+	connection.Start(servers);
+}
+
+__attribute__((destructor)) static void destructor(void)
+{
+	system("leaks -q webserv");
+}
