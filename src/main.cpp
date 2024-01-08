e2df62de6c6d8b11d5e7b5409ac158259a7b6b78
Yui Nishimura <nisshi.yui79@gmail.com>
2024-01-08 19:22:38 +0900

    Refactor main.cpp and fix argument handling

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,33 @@
-#include "server/connection.hpp"
 #include "config/parseConfig.hpp"
+#include "server/connection.hpp"
 #include "utils/utils.hpp"
 
 int main(int argc, char **argv)
 {
-	const char *config_path;
-	if (argc < 2)
-	    config_path = "config/default.conf";
-	else if (argc == 2)
-		config_path = argv[1];
-	else
-	{
-		utils::printError("引数が多すぎます");
-		return 1;
-	}
+    const char *config_path;
+    if (argc < 2)
+        config_path = "config/default.conf";
+    else if (argc == 2)
+        config_path = argv[1];
+    else
+    {
+        utils::printError("引数が多すぎます");
+        return 1;
+    }
 
-	ParseResult result = ParseConfig(config_path);
-	if (!result.ok())
-	{
-		utils::printError(result.unwrapErr());
-		return 1;
-	}
-	std::vector<Server> servers = result.unwrap();
+    ParseResult result = ParseConfig(config_path);
+    if (!result.ok())
+    {
+        utils::printError(result.unwrapErr());
+        return 1;
+    }
+    std::vector<Server> servers = result.unwrap();
 
-	Connection connection;
-	connection.Start(servers);
+    Connection connection;
+    connection.Start(servers);
 }
 
 __attribute__((destructor)) static void destructor(void)
 {
-	system("leaks -q webserv");
+    system("leaks -q webserv");
 }
