64682db4ba4c7575f393ed6f8f6f1b4e79d428d9
Yui Nishimura <nisshi.yui79@gmail.com>
2024-01-08 16:00:05 +0900

    Fix include paths and simplify shouldUseCgi function

--- srcs/cgi/determineCGI.cpp
+++ srcs/cgi/determineCGI.cpp
@@ -1,16 +1,9 @@
-#include "../HTTPRequest/HTTPParser.hpp"
-#include "../config/config.hpp"
+#include "../HttpRequest/RequestParser.hpp"
+#include "../utils/utils.hpp"
 
 bool shouldUseCgi(const ParsedRequest request, const Server server)
 {
     const Location location = utils::matchedLocation(request.uri, server.locations);
-    const std::vector<std::string> extensions = location.;
-    for (unsigned int i = 0; i < extensions.size(); i++)
-    {
-        const std::string extension = extensions[i];
-        const std::string uriExtension = request.uri.substr(request.uri.find_last_of(".") + 1);
-        if (extension == uriExtension)
-            return true;
-    }
-    return false;
+    const std::string uriExtension = request.uri.substr(request.uri.find_last_of(".") + 1);
+    return uriExtension == location.cgiExtension;
 }
