6ac58aede23028a0e5cb28b874ec4e066f92f0ac
Yui Nishimura <nisshi.yui79@gmail.com>
2024-01-09 16:33:42 +0900

    Add statusCode, headers, and body to HttpResponse struct

--- src/HttpResponse/HttpResponse.hpp
+++ src/HttpResponse/HttpResponse.hpp
@@ -3,12 +3,15 @@
 
 #include <cstring>
 #include <iostream>
+#include <map>
 
 const char *CRLF = "\r\n";
 
 struct HttpResponse
 {
-    ;
+    const int statusCode;
+    const std::map<std::string, std::string> headers;
+    const std::string body;
 };
 
 #endif
