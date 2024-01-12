163c868ffa8859ec5df7bb9a465f7c7cad0fd8a6
Subaru Sasaki <subarunrun0812@yahoo.co.jp>
2024-01-12 12:48:34 +0900

    fix: token.valuesがからの場合、removeSemicolonFromToken()が呼び出されない

--- src/config/tokenizeConfig.hpp
+++ src/config/tokenizeConfig.hpp
@@ -3,22 +3,6 @@
 
 #include "parseConfig.hpp"
 
-#define SERVER "server"
-#define SERVER_NAME "server_name"
-#define LISTEN "listen"
-#define LOCATION "location"
-#define ALLOW_METHOD "allow_method"
-#define CGI_EXECUTOR "cgi_executor"
-#define UPLOAD_PATH "upload_path"
-#define RETURN "return"
-#define ROOT "root"
-#define ERROR_PAGE "error_page"
-#define CLIENT_MAX_BODY_SIZE "client_max_body_size"
-#define INDEX "index"
-#define AUTOINDEX "autoindex"
-#define OPEN_BRACKET "{"
-#define CLOSE_BRACKET "}"
-
 struct Tokenize
 {
     std::string key;
@@ -28,7 +12,7 @@ struct Tokenize
 typedef utils::Result<std::vector<Tokenize>, std::string> tokenizeResult;
 typedef utils::Result<std::string, std::string> stringResult;
 
-// stringResult checkBracketsBalance(const std::vector<Tokenize> &tokens);
+stringResult checkBracketsBalance(const std::vector<Tokenize> &tokens);
 stringResult removeSemicolonFromToken(Tokenize &token);
 tokenizeResult tokenize(const char *configPath);
 
