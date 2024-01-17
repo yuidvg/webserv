8ab74bb51b3dbd94fb7f781648796f6c89a0f192
Subaru Sasaki <subarunrun0812@yahoo.co.jp>
2024-01-17 21:33:07 +0900

    移動前のファイルを削除

--- src/httpRequest/HttpRequest.cpp
+++
@@ -1,7 +0,0 @@
-#include "HttpRequest.hpp"
-
-HttpRequest::HttpRequest(){};
-
-HttpRequest::HttpRequest(const std::string m, const std::string u, const std::string v, const Headers h,
-                         const std::string b)
-    : method(m), uri(u), version(v), headers(h), body(b){};
