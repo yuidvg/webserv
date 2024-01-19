13a984873c7108d783350b870f654d71bc4b00ec
Minato Abe <m.abe.howcollect@gmail.com>
2024-01-19 10:09:55 +0900

    fix: 関数名

--- src/httpRequest/parse.cpp
+++ src/httpRequest/parse.cpp
@@ -27,7 +27,7 @@ static bool isLineTooLong(const std::string &line)
     return (false);
 }
 
-static int isValidRequestLine(RequestLine requestLine)
+static int getRequestLineStatusCode(RequestLine requestLine)
 {
     if (requestLine.uri.find(':') != std::string::npos &&
         requestLine.uri.find('*') != std::string::npos) // CONNECT, OPTIONSは非対応
@@ -91,7 +91,7 @@ ParseRequestLineResult parseHttpRequestLine(std::istream &httpRequest)
         return (ParseRequestLineResult::Error(getRequestLineResult.error));
 
     /* エラーチェック */
-    int statusCode = isValidRequestLine(getRequestLineResult.value);
+    int statusCode = getRequestLineStatusCode(getRequestLineResult.value);
 
     return (statusCode != SUCCESS) ? ParseRequestLineResult::Error(statusCode)
                                    : ParseRequestLineResult::Success(getRequestLineResult.value);
