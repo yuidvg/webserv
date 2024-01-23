708376fddbd76f2cf024f3ae68cc38f81ddc429a
Subaru Sasaki <subarunrun0812@yahoo.co.jp>
2024-01-23 18:53:55 +0900

    add: errorPageを作成する関数

---
+++ src/functions/errorPage.cpp
@@ -0,0 +1,45 @@
+#include "build.hpp"
+
+static HttpResponse createHttpDefaultErrorPage(const unsigned int statusCode)
+{
+    if (statusCode <= 400 && statusCode < 500)
+        return BAD_REQUEST_RESPONSE;
+    else
+        return SERVER_ERROR_RESPONSE;
+}
+
+HttpResponse createHttpErrorPage(const HttpResponse &httpresponse, const Servers &servers, const Sd &sd)
+{
+    const unsigned int statusCode = httpresponse.statusCode;
+    if (host == NULL)
+        return createHttpDefaultErrorPage(statusCode);
+    const MatchedServerResult serverResult = matchedServer(httpresponse.host, servers, sd);
+    if (!serverResult.success)
+        return createHttpDefaultErrorPage(statusCode);
+
+    std::map<int, std::string>::const_iterator it = serverResult.value.errorPages.find(statusCode);
+
+    if (it != server.errorPages.end())
+    {
+        std::ifstream file(it->second);
+        if (file)
+        {
+            std::string content;
+            std::string line;
+            while (std::getline(file, line))
+            {
+                content += line;
+            }
+            file.close();
+            return HttpResponse(statusCode, Headers("text/html", utils::toString(content.length())), content);
+        }
+        else
+        {
+            return createHttpDefaultErrorPage(statusCode);
+        }
+    }
+    else
+    {
+        return createHttpDefaultErrorPage(statusCode);
+    }
+}
---
+++ src/httpResponse/buildHttpResponse.cpp
@@ -0,0 +1,66 @@
+#include "../config/find.hpp"
+#include "build.hpp"
+namespace
+{
+bool isMethodAllowed(const HttpRequest &request, const Location &location)
+{
+    for (std::vector<std::string>::const_iterator it = location.allowMethods.begin(); it != location.allowMethods.end();
+         ++it)
+        if (*it == request.method)
+            return true;
+    return false;
+}
+HttpResponse responseToValidRequest(const HttpRequest &request, const Server &server)
+{
+    const Location location = matchedLocation(request.target, server.locations);
+    if (isMethodAllowed(request, location))
+    {
+        if (request.method == "GET")
+            return conductGet(request, location);
+        else if (request.method == "POST")
+            return conductPost(request, location);
+        else if (request.method == "DELETE")
+            return conductDelete(request, location);
+        else
+            return METHOD_NOT_ALLOWED_RESPONSE("GET, POST, DELETE");
+    }
+    else
+    {
+        return METHOD_NOT_ALLOWED_RESPONSE(utils::join(location.allowMethods, ", "));
+    }
+}
+} // namespace
+
+HttpResponse response(const ParseRequestResult &requestResult, const Sd &sd, const Servers &servers)
+{
+    if (requestResult.success)
+    {
+        const HttpRequest request = requestResult.value;
+        const MatchedServerResult serverResult = matchedServer(request.host, servers, sd);
+        if (serverResult.success)
+        {
+            const Server server = serverResult.value;
+            return responseToValidRequest(requestResult.value, server);
+        }
+        else
+        {
+
+            return createHttpErrorPage(serverResult.error, servers, sd);
+        }
+    }
+    else
+    {
+        return createHttpErrorPage(requestResult.error, servers, sd);
+    }
+}
+
+std::string responseText(const HttpResponse &response)
+{
+    std::string text = SERVER_PROTOCOL + " " + std::to_string(response.statusCode) + CRLF;
+    for (std::map<std::string, std::string>::const_iterator it = response.headers.begin(); it != response.headers.end();
+         ++it)
+        text += it->first + ": " + it->second + CRLF;
+    text += CRLF;
+    text += response.body;
+    return text;
+}
