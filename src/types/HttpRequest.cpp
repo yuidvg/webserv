b56238f69cf44eedf67bec10bfc29e5b82328f49
Minato Abe <m.abe.howcollect@gmail.com>
2024-01-23 14:49:54 +0900

    add: a member variable of httpRequest, 'host'

--- src/types/HttpRequest.cpp
+++ src/types/HttpRequest.cpp
@@ -2,6 +2,6 @@
 
 HttpRequest::HttpRequest(){};
 
-HttpRequest::HttpRequest(const std::string m, const std::string u, const std::string v, const Headers h,
-                         const std::string b)
-    : method(m), uri(u), version(v), headers(h), body(b){};
+HttpRequest::HttpRequest(const std::string m, const std::string t, const std::string v, const Headers h,
+                         const std::string b, const std::string host)
+    : method(m), target(t), version(v), headers(h), body(b), host(host){};
