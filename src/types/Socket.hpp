e3b098c00569a7cbf067e00762a39ad763eab3f5
Subaru Sasaki <subarunrun0812@yahoo.co.jp>
2024-01-26 22:10:57 +0900

    fix: segumention faultを修正
    初期化時の値が型違いでエラーが発生した。

--- src/types/Socket.hpp
+++ src/types/Socket.hpp
@@ -7,8 +7,8 @@ struct Socket
     const std::string opponentIp;
     const unsigned int opponentPort;
 
-    Socket() : descriptor(0), port(0), opponentIp(0), opponentPort(0){};
-    Socket(const unsigned int &descriptor, const unsigned int &port, const std::string &opponentIp = 0,
+    Socket() : descriptor(0), port(0), opponentIp(""), opponentPort(0){};
+    Socket(const unsigned int &descriptor, const unsigned int &port, const std::string &opponentIp = "",
            const unsigned int &opponentPort = 0)
         : descriptor(descriptor), port(port), opponentIp(opponentIp), opponentPort(opponentPort){};
     bool operator==(const Socket &other) const
