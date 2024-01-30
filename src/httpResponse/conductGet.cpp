0e4247c91485c87b530cb2cf1d6d72c227e15dab
Subaru Sasaki <subarunrun0812@yahoo.co.jp>
2024-01-30 17:40:16 +0900

    refactor: redirect

--- src/httpResponse/conductGet.cpp
+++ src/httpResponse/conductGet.cpp
@@ -4,11 +4,11 @@
 
 HttpResponse conductGet(const HttpRequest &request, const Location &location)
 {
-    const std::string targetResourcePath = comply(request.target, location);
     if (!location.redirect.empty())
     {
         return redirectResponse(location.redirect);
     }
+    const std::string targetResourcePath = comply(request.target, location);
     const IsDirectoryResult isDirectoryResult = utils::isDirectory(targetResourcePath);
     if (isDirectoryResult.success)
     {
