8e62c687540135df48fa3e616d1f0c44bdef6089
Subaru Sasaki <subarunrun0812@yahoo.co.jp>
2024-01-31 15:13:24 +0900

    fix: defaultのerror pageのバグ issue#51

--- src/globals.hpp
+++ src/globals.hpp
@@ -45,15 +45,15 @@ const int MAX_LEN = 8192;
 
 // httpResponse
 const HttpResponse BAD_REQUEST_RESPONSE =
-    HttpResponse(BAD_REQUEST, Headers("text/html", "182"),
+    HttpResponse(BAD_REQUEST, Headers("text/html", "181"),
                  " <html> <head> <title>400 Bad Request</title> </head> <body> <h1>400 Bad Request</h1> <p>Your "
-                 "browser sent a request that this server could not understand.<br /></p> </body> </html>");
+                 "browser sent a request that this server could not understand.<br/></p> </body> </html>");
 
 const HttpResponse SERVER_ERROR_RESPONSE =
-    HttpResponse(SERVER_ERROR, Headers("text/html", "237"),
+    HttpResponse(SERVER_ERROR, Headers("text/html", "236"),
                  " <html> <head> <title>500 Internal Server Error</title> </head> <body> <h1>500 Internal Server "
                  "Error</h1> <p>The server encountered an internal error or misconfiguration and was unable to "
-                 "complete your request.<br /></p> </body> </html>");
+                 "complete your request.<br/></p> </body> </html>");
 
 const HttpResponse SUCCESS_RESPONSE = HttpResponse(SUCCESS, Headers("text/html", "0"), "");
 
