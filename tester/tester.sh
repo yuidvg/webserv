3df4b681d3d0488fea59ec1f23707c9f126a773d
M1n01 <abe.minato.bz@gmail.com>
2024-04-27 13:49:47 +0900

    fix: testerファイル分割

--- tester/tester.sh
+++ tester/tester.sh
@@ -6,46 +6,7 @@ YELLOW="\033[33m"
 NORMAL="\033[0m"
 SERVER_ADDRESS="${1:-localhost:8080}"
 
-curl_commands=(
-    "curl -X GET http://${SERVER_ADDRESS}/ 200"
-    "curl -X POST -d \"nickname=test\" http://${SERVER_ADDRESS}/uploadPath 200"
-    "curl -X DELETE http://${SERVER_ADDRESS}/uploadPath/uploadPath.txt 200"
-    "curl -X GET http://${SERVER_ADDRESS}/autoindex 200"
-    "curl -X GET http://${SERVER_ADDRESS}/nothing 400"
-    "curl -X GET http://${SERVER_ADDRESS}/nothing 400"
-    "curl -X POST -d \"nickname=test\" http://${SERVER_ADDRESS}/nothing 400"
-    "curl -X DELETE http://${SERVER_ADDRESS}/uploadPath/nothing 400"
-    "curl -X GET http://${SERVER_ADDRESS}/ -H 'Host: ${SERVER_ADDRESS}' 200"
-    "curl -X GET http://${SERVER_ADDRESS}/ -H 'Host:${SERVER_ADDRESS}' 400"
-    "curl -X POST http://${SERVER_ADDRESS}/uploadPath/test -H 'Host: ${SERVER_ADDRESS}' -H 'Content-Length: 11' -d \"Hello World\" 200"
-    "curl -X POST http://${SERVER_ADDRESS}/uploadPath/test -H 'Host: ${SERVER_ADDRESS}' -H 'Content-Length: -9' -d \"123456789\" 400"
-    "curl -X POST http://${SERVER_ADDRESS}/uploadPath/test -H 'Host: ${SERVER_ADDRESS}' -H 'Content-Length: 100' -d \"123456789\" 400"
-    "curl -X GET http://${SERVER_ADDRESS}/cgi-bin/helloWorld.cgi 200"
-    "curl -X GET http://${SERVER_ADDRESS}/cgi-bin/redirAfterFiveMinutes.cgi 200"
-    "curl -X GET http://${SERVER_ADDRESS}/cgi-bin/submit.cgi 200"
-    # "curl -X GET http://${SERVER_ADDRESS}/cgi-bin/remoteRedirect.cgi -L 302"
-    # "curl -X GET http://${SERVER_ADDRESS}/cgi-bin/localRedirect.cgi -L 302"
-)
-
-function run_and_check_curl_command() {
-    local cmd="$1"
-    local expected_code=${cmd##* }
-
-    # -s でサイレントモード、-w でHTTPステータスコード出力
-    http_code=$(eval ${cmd% *} -s -o response.txt -w "%{http_code}")
-    if [[ $http_code -eq $expected_code ]]; then
-        echo -en "${GREEN}OK (${http_code})"
-    else
-        echo -en "${RED}KO (${http_code})"
-    fi
-    echo -ne "${NORMAL}"
-    echo " -> ${cmd}"
-}
-
-echo "Running curl commands..."
-for cmd in "${curl_commands[@]}"; do
-    run_and_check_curl_command "$cmd"
-done
+./tester/normalTest.sh
 
 # その他のコマンド
 other_commands=(
