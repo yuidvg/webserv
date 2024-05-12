a52eab69319e055c060289ecae07559ed52a6349
Yui Nishimura <nisshi.yui79@gmail.com>
2024-05-12 17:20:08 +0900

    removed unused function "readFile"

--- src/prototypes/utils.hpp
+++ src/prototypes/utils.hpp
@@ -13,10 +13,9 @@ std::string lowerCase(const std::string str);
 bool isNumber(const std::string str);
 std::string hexToUtf8Char(const std::string &hex);
 std::string getlineCustom(std::istringstream &requestTextStream);
-std::vector<std::string> split(const std::string &original, const std::string &delim);
+std::vector< std::string > split(const std::string &original, const std::string &delim);
 std::string removeCharacter(const std::string str, const char charToRemove);
 StringToIntResult stringToInt(const std::string &str, int minVal, int maxVal);
-ReadFileResult readFile(const int fd);
 std::string itoa(const int &num);
 bool isHex(const std::string &str);
 
@@ -28,11 +27,11 @@ bool isAbsoluteUri(const std::string &uri);
 FileContentResult fileContent(const std::string &path);
 std::string contentType(const std::string &path);
 bool isDirectory(const std::string &path);
-Option<Socket> createFile(const std::string &fileName, const std::string &path);
+Option< Socket > createFile(const std::string &fileName, const std::string &path);
 bool writeToFile(const std::string &path, const std::string &content);
 
 // vector
-std::string join(const std::vector<std::string> &list, const std::string &delimiter);
+std::string join(const std::vector< std::string > &list, const std::string &delimiter);
 
 // status
 bool isStatusInRange(const int status);
