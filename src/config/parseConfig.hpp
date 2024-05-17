c808bf0bb6163187747a0b13302fcafe94a8cdad
Yui Nishimura <nisshi.yui79@gmail.com>
2024-05-17 20:06:56 +0900

    cpl error

--- src/config/parseConfig.hpp
+++ src/config/parseConfig.hpp
@@ -1,11 +1,11 @@
 #pragma once
 
+#include "../globals.hpp"
 #include "../types/bottom.hpp"
-#include "globals.hpp"
 
 namespace parseConfig
 {
 ConfigResult parseConfig(const char *configPath);
-ParseServerResult parseServerContext(std::vector<std::string> &tokens);
-ParseLocationResult parseLocationContext(std::vector<std::string> &tokens, std::string &locationPath);
+ParseServerResult parseServerContext(std::vector< std::string > &tokens);
+ParseLocationResult parseLocationContext(std::vector< std::string > &tokens, std::string &locationPath);
 } // namespace parseConfig
